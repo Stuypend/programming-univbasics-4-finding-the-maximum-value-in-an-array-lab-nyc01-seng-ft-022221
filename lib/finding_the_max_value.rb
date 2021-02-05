5d243ecda3aaf967b2e04b5f3c0ba455bcd25462
Andre Powers <andre@stuypend.com>
2021-02-05 15:14:22 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,6 +3,7 @@ def find_max_value(array)
   index = 0
   while(index < array.length) do
     biggest =  array[index] > array[biggest] ? index : biggest
+    index += 1
   end
   return array[biggest]
 end
